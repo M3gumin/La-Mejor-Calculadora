0d3bc0874a271d0ee9ad88acc950bfea1f746e75
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2023-05-19 10:01:11 +0200

    Map

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,5 +1,21 @@
 public class Calculadora {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        while (true) {
+            //Calculadora 
+        }
+
     }
+
+    //! Funciones principales de la calculadora
+    // Suma +
+    // Resta -
+    // Multiplicacion *
+    // Division /
+    // Potencia **
+    // Raiz -*
+    // Factorial !
+    // Logaritmo log()
+    // Valor Absoluto ||
+
 }
