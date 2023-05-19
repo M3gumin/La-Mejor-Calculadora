b0d3b6414fd45e7d55c720baee0488c9e83bd275
JbLuffy <jaimebmazul@gmail.com>
2023-05-19 13:17:17 +0200

    cambio suma

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,10 +1,18 @@
 public class Calculadora {
     public static void main(String[] args) throws Exception {
+    	
+    	int a=2, b=1;
         
         while (true) {
             //Calculadora 
         }
+        
+        suma(a, b);
 
+    }
+    
+    public static int suma(int a, int b) {
+    	return (a+b);
     }
 
     //! Funciones principales de la calculadora
